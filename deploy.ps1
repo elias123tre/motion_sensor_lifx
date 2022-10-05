15acfccec72d93ac358cc0cd9c8b1c57ffd37352
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2022-10-06 01:00:03 +0200

    Set write permission on upload in deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -2,9 +2,11 @@ Write-Host "Building in release mode..."
 cargo build --release
 Write-Host "Stopping running pir systemd service..."
 ssh pi "sudo systemctl stop pir"
+Write-Host "Adding write permission on binary..."
+ssh pi "sudo chmod a+w /home/pi/motion_sensor_lifx"
 Write-Host "Copying release binary to network drive..."
 robocopy /NFL /NDL /NJH /NJS /nc /ns "./target/arm-unknown-linux-gnueabihf/release" "\\raspi\RaspberryPi\home\pi" motion_sensor_lifx
-Write-Host "Add execution permission to file..."
+Write-Host "Adding execution permission to file..."
 ssh pi "sudo chmod a+x /home/pi/motion_sensor_lifx"
 Write-Host "Restarting pirtimer systemd service..."
 ssh pi "sudo systemctl restart pir"
